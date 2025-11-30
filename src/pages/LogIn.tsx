72b6ca9ce8e60ec1bbe6796bd384400a4e2c57d0
emkataumre <emo.vladinov@gmail.com>
2025-11-30 17:30:34 +0100

    tracking remember me state

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -105,6 +105,7 @@ export function LoginPage() {
                 type="checkbox"
                 id="remember"
                 checked={rememberMe}
+                onChange={(e) => setRememberMe(e.target.checked)}
                 className="h-4 w-4 rounded border-gray-300 accent-primary focus:ring-primary"
               />
               <Label htmlFor="remember">Remember me</Label>
