6d2f20da8e612bc0547f48d54fb5c8b765debfa1
emkataumre <emo.vladinov@gmail.com>
2025-12-01 15:06:45 +0100

    removed test protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { Route, Routes } from 'react-router'
 import { LoginPage } from './pages/LogIn'
 import { SignUpPage } from './pages/SignUp'
-import { ProtectedRoute } from './routes/ProtectedRoute'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<p>Home</p>} />
-        </Route>
+        <Route path="/" element={<p>Home</p>} />
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/signup" element={<SignUpPage />}></Route>
       </Routes>
